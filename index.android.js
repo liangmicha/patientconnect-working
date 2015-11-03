a0442688886c6c89bed57188bc2b816cfb48b3b2
Michael Liang <liangmicha@gmail.com>
2015-11-02 22:56:13 -0800

    parse react import successful

--- index.android.js
+++ index.android.js
@@ -1,4 +1,7 @@
 var React = require('react-native');
+var Parse = require('parse/react-native');
+var ParseReact = require('parse-react/react-native');
+
 import { AppRegistry } from 'react-native';
 import LandingCard from './src/components/LandingCard'
 
