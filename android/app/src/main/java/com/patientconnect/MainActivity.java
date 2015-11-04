d6aecac40e2f17a627c35828e05ec7125bdb73e9
Michael Liang <liangmicha@gmail.com>
2015-11-04 00:12:33 -0800

    oman push notifications

--- android/app/src/main/java/com/patientconnect/MainActivity.java
+++ android/app/src/main/java/com/patientconnect/MainActivity.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 
+import com.parse.ParseInstallation;
+import com.parse.Parse;
 import com.facebook.react.LifecycleState;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactRootView;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.learnium.RNDeviceInfo.*;
 
 public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
 
@@ -19,6 +22,8 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Parse.initialize(this, "WTAN135mlF1cqo5eL7IRFpDKztxSRdPKLZWmrcOL", "a3EOsvfKbGQDFLUIDyzd26pTCYvI4VGURcsXR8bj");
+        ParseInstallation.getCurrentInstallation().saveInBackground();
         mReactRootView = new ReactRootView(this);
 
         mReactInstanceManager = ReactInstanceManager.builder()
@@ -27,6 +32,7 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
                 .setJSMainModuleName("index.android")
                 .addPackage(new MainReactPackage())
                 .addPackage(new ComponentPackager())
+                .addPackage(new RNDeviceInfo())
                 .setUseDeveloperSupport(BuildConfig.DEBUG)
                 .setInitialLifecycleState(LifecycleState.RESUMED)
                 .build();
--- index.android.js
+++ index.android.js
@@ -25,6 +25,26 @@ Parse.initialize(
 	"YtzjUMxuYOAAVPERLWbuhc3fXVHQk6iHoiPb1XJR"
 );
 
+var DeviceInfo = require('react-native-device-info');
+
+console.log("Device Unique ID", DeviceInfo.getUniqueID());  // e.g. FCDBD8EF-62FC-4ECB-B2F5-92C9E79AC7F9
+
+console.log("Device Manufacturer", DeviceInfo.getManufacturer());  // e.g. Apple
+
+console.log("Device Model", DeviceInfo.getModel());  // e.g. iPhone
+
+console.log("Device Name", DeviceInfo.getSystemName());  // e.g. iPhone OS
+
+console.log("Device Version", DeviceInfo.getSystemVersion());  // e.g. 9.0
+
+console.log("Bundle Id", DeviceInfo.getBundleId());  // e.g. com.learnium.mobile
+
+console.log("Build Number", DeviceInfo.getBuildNumber());  // e.g. 89
+
+console.log("App Version", DeviceInfo.getVersion());  // e.g. 1.1.0
+
+console.log("App Version (Readable)", DeviceInfo.getReadableVersion());  // e.g. 1.1.0.89
+
 var App = React.createClass({
 	mixins: [ParseReact.Mixin],
 	observe: function(props, state) {
@@ -33,6 +53,7 @@ var App = React.createClass({
 		};
 	},
 	render: function() {
+
 		if (this.data.user) {
 			return (
 				<MainView user={this.data.user}>
